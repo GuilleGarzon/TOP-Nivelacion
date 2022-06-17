bb5239d93e8a8197ca4013216ebedd3cb3d28ad6
Guillermo Garzon <guillegarzon.gg@gmail.com>
2022-06-17 18:19:26 -0500

    Agregar carpeta Javascript

---
+++ JavaScript/Manipulacion DOM/app.js
@@ -0,0 +1,9 @@
+// 1. Inserta 4 items más con el texto 3, 4, 5 y 6 respectivamente dentro del contenedor.
+
+// 2. Agrégales a todos los items la clase error
+
+// 3. Agrégale el texto "2" al círculo
+
+// 4. Elimina el párrafo
+
+
