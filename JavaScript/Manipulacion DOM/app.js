be6b1984b189c1fb108c3a687ab2b780d24828b0
Guillermo Garzon <guillegarzon.gg@gmail.com>
2022-06-17 19:18:57 -0500

    Agregar solucion de manipulacion del DOM

--- JavaScript/Manipulacion DOM/app.js
+++ JavaScript/Manipulacion DOM/app.js
@@ -1,9 +1,27 @@
 // 1. Inserta 4 items más con el texto 3, 4, 5 y 6 respectivamente dentro del contenedor.
 
+const $container = document.querySelector(".container")
+
+for (let i = 3; i < 7; i++) {    
+    let div = document.createElement("div")
+    div.classList.add("item")
+    div.textContent = i;
+    
+    $container.appendChild(div)
+}
+
 // 2. Agrégales a todos los items la clase error
 
+let $items = document.querySelectorAll('.item')   
+    $items.forEach(item => item.classList.add('error'))
+
+   
 // 3. Agrégale el texto "2" al círculo
+
+let $circle = document.querySelector('.circle');
+$circle.textContent = 2;
 
 // 4. Elimina el párrafo
 
-
+let $parrafo = document.querySelector('p')
+$parrafo.parentNode.removeChild($parrafo);
